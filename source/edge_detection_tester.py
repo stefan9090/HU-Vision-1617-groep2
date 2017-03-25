b0eb76eb3f8fcd7fea61f604e9646b0d2e6a872d
Jeroen Kok <Jeroen.kok@student.hu.nl>
2017-03-25 17:25:56 +0100

    Changed the code a little
    Added a counter that prints at what amount we are every so many runs.
    Added absolute path on my end

--- source/edge_detection_tester.py
+++ source/edge_detection_tester.py
@@ -1,15 +1,17 @@
 import sys, os
 
-exe_path = "C:\\Users\\stefan-1212\\Desktop\\vision2\\HU-Vision-1617-groep2\\source\\ExternalDLL\\Release\\ExternalDLL.exe"
-
+#exe_path = "C:\\Users\\stefan-1212\\Desktop\\vision2\\HU-Vision-1617-groep2\\source\\ExternalDLL\\Release\\ExternalDLL.exe"
+exe_path = "E:\\Users\Jeroen\\Documents\\Gitkraken\\HU-Vision-1617-groep2\\source\\ExternalDLL\\Release\\ExternalDLL.exe"
 
 
 amount = 10000
 var_list = []
 for i in range(amount):
     var_list.append(os.system(exe_path))
-for i in var_list:
-    print(i/1000)
+    if i%100==0:
+        print(i+1)
+#for i in var_list:
+#    print(i/1000)
 print()
 print("max =",max(var_list)/1000)
 print("min =",min(var_list)/1000)
